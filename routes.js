f31f2e990e6720c2d44ce5cc8e6e3892a2aae5cc
Jozef Szumilo <jozef.szumilo@gmail.com>
2021-02-06 11:18:40 +0100

    routes fro individuals

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const individuals = require('./lib/individuals');
 
 const router = express.Router();
 
@@ -6,5 +7,11 @@ router.get('/', (req, res) => {
     console.log('checking the service state');
     res.send('Alive');
 });
+
+router.post('/login', (req, res) => {
+    res.send('TODO');
+});
+
+router.use('/individuals', individuals);
 
 module.exports = router;
