77f1ac99ff8dbb8bb139b7e30fd197c9f716ffc8
Jozef Szumilo <jozef.szumilo@gmail.com>
2021-02-07 20:18:08 +0100

    database setup and tare down scripts and fixes

--- database/release001.sh
+++ database/release001.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-echo "Creating Individual status"
-sqlite3 bank.sqlite < database/migrations/001-CREATE-INDIVIDUAL-STATUS.sql
-
-echo "Creating Individual"
-sqlite3 bank.sqlite < database/migrations/002-CREATE-INDIVIDUAL.sql
+echo "Creating Individual status and individual tables"
+sqlite3 prisma/bank.sqlite < prisma/migrations/20210207154559_r1_individuals_sql/migration.sql
 
 echo "Loading individual status dictionary"
-sqlite3 bank.sqlite < database/seed/individual-status.dictionaty.sql 
+sqlite3 prisma/bank.sqlite < database/seed/individual-status.dictionaty.sql 
 
 echo "Loading the master and test user to individuals"
-sqlite3 bank.sqlite < database/seed/individual.base.sql 
+sqlite3 prisma/bank.sqlite < database/seed/individual.base.sql 
